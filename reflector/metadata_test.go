d80fcd735d4398355b7a3d7295202605e8eeb51b
ionous <github@ionous.net>
2017-06-26 11:45:29 -0700

    more tag parsing
    in prep for sharing class/registry code

--- reflector/metadata_test.go
+++ reflector/metadata_test.go
@@ -1,41 +1,99 @@
 package reflector
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	r "reflect"
 	"testing"
 )
 
 func TestMetadata(t *testing.T) {
-	assert := assert.New(t)
-	//`if:"id,plural:base classes"`
-	MakeMetadata := func(tag string) (out Metadata) {
-		s := r.StructTag(tag).Get("if")
-		if len(s) > 0 {
-			out = make(Metadata)
-			out.AddString(s, "id")
+	suite.Run(t, new(MetadataSuite))
+}
+
+type MetadataSuite struct {
+	suite.Suite
+}
+
+func (t *MetadataSuite) MakeMetadata(tag string) (out Metadata) {
+	s := MakeTag(r.StructTag(tag))
+	if len(s) > 0 {
+		out = make(Metadata)
+		out.AddString(s, "fill")
+	}
+	return
+}
+
+func (t *MetadataSuite) TestKey() {
+	m := t.MakeMetadata(`if:"id"`)
+	t.Len(m, 1)
+	t.Equal("fill", m["id"])
+}
+func (t *MetadataSuite) TestKeyValue() {
+	m := t.MakeMetadata(`if:"plural:tests"`)
+	t.Len(m, 1)
+	t.Equal("tests", m["plural"])
+}
+func (t *MetadataSuite) TestMultipleKeys() {
+	m := t.MakeMetadata(`if:"id,plural:tests"`)
+	t.Len(m, 2)
+	t.Equal("fill", m["id"])
+	t.Equal("tests", m["plural"])
+}
+func (t *MetadataSuite) TestMissingCommas() {
+	m := t.MakeMetadata(`if:"edge:test:tests"`)
+	t.Len(m, 1)
+	t.Equal("test:tests", m["edge"])
+}
+
+func TestMetaTags(t *testing.T) {
+	suite.Run(t, new(MetaTagSuite))
+}
+
+type MetaTagSuite struct {
+	suite.Suite
+}
+
+func (t *MetaTagSuite) TestInvalidTag() {
+	tag := MakeTag(r.StructTag(`fi:"id"`))
+	t.EqualValues("", tag)
+}
+func (t *MetaTagSuite) TestKey() {
+	tag := MakeTag(r.StructTag(`if:"id"`))
+	if id, ok := tag.Find("id"); t.True(ok) {
+		t.Empty(id)
+	}
+}
+func (t *MetaTagSuite) TestSimilarTag() {
+	tag := MakeTag(r.StructTag(`if:"iddy:5"`))
+	_, ok := tag.Find("id")
+	t.False(ok)
+}
+func (t *MetaTagSuite) TestKeyValue() {
+	tag := MakeTag(r.StructTag(`if:"plural:tests"`))
+	if _, ok := tag.Find("id"); t.False(ok) {
+		//
+		if res, ok := tag.Find("plural"); t.True(ok) {
+			t.Equal("tests", res)
 		}
-		return
 	}
-	{
-		m := MakeMetadata(`if:"id"`)
-		assert.Len(m, 1)
-		assert.Equal("id", m["id"])
+}
+func (t *MetaTagSuite) TestMultipleKeys() {
+	tag := MakeTag(r.StructTag(`if:"id,other:true,plural:tests"`))
+	if id, ok := tag.Find("id"); t.True(ok) {
+		t.Empty(id)
+
+		if other, ok := tag.Find("other"); t.True(ok) {
+			t.Equal("true", other)
+			//
+			if res, ok := tag.Find("plural"); t.True(ok) {
+				t.Equal("tests", res)
+			}
+		}
 	}
-	{
-		m := MakeMetadata(`if:"plural:tests"`)
-		assert.Len(m, 1)
-		assert.Equal("tests", m["plural"])
-	}
-	{
-		m := MakeMetadata(`if:"id,plural:tests"`)
-		assert.Len(m, 2)
-		assert.Equal("id", m["id"])
-		assert.Equal("tests", m["plural"])
-	}
-	{
-		m := MakeMetadata(`if:"edge:test:tests"`)
-		assert.Len(m, 1)
-		assert.Equal("test:tests", m["edge"])
+}
+func (t *MetaTagSuite) TestMissingCommas() {
+	tag := MakeTag(r.StructTag(`if:"edge:test:tests"`))
+	if res, ok := tag.Find("edge"); t.True(ok) {
+		t.Equal("test:tests", res)
 	}
 }
--- reflector/tags.go
+++ reflector/tags.go
@@ -7,8 +7,54 @@ import (
 
 type Metadata map[string]string
 
+func MakeTag(f r.StructTag) Tag {
+	return Tag(f.Get("if"))
+}
+
+type Tag string
+
+func (t Tag) String() string {
+	return string(t)
+}
+
+func (t Tag) Split() []string {
+	s := string(t)
+	return strings.Split(s, ",")
+}
+
+func (t Tag) Find(key string) (ret string, okay bool) {
+	s := string(t)
+	for {
+		// find key in the string;
+		// do this in a loop to handle errant substring matches: "iffy:" vs "if:"
+		if i := strings.Index(s, key); i < 0 {
+			break
+		} else {
+			// evaluate everything after
+			s = s[i+len(key):]
+			// at the end of string, or a separator then we have a key without value
+			if len(s) == 0 || s[0] == ',' {
+				okay = true
+				break
+			} else {
+				// after the key is a value separator? extract the value
+				if s[0] == ':' {
+					if end := strings.Index(s, ","); end < 0 {
+						ret = s[1:] // no new separator before the end, use everything after the :
+					} else {
+						ret = s[1:end] // found a separator before the end, split out our value
+					}
+					okay = true // either way, we are done.
+					break
+				}
+			}
+		}
+	}
+	return
+}
+
 func MergeMetadata(f r.StructField, m *Metadata) {
-	if s := f.Tag.Get("if"); len(s) > 0 {
+	if s := MakeTag(f.Tag); len(s) > 0 {
 		if len(*m) == 0 {
 			*m = make(Metadata)
 		}
@@ -22,11 +68,12 @@ func MergeMetadata(f r.StructField, m *Metadata) {
 // Tags fields are of the format //`if:"key:value"`
 // The tagcontents is the part inside the double quotes.
 // The fill is used when the value part is empty or missing.
-func (m Metadata) AddString(tagcontents, fill string) {
-	parts := strings.Split(tagcontents, ",")
+func (m Metadata) AddString(tagcontents Tag, fill string) {
+	parts := tagcontents.Split()
 	for _, s := range parts {
 		if len(s) > 0 {
 			kv := strings.Split(s, ":")
+			// no kv?
 			if len(kv) == 1 {
 				k, v := kv[0], fill
 				m[k] = v
