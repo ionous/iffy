485f80d07a7ed5afc3406c3df976c9b0e4461bf2
ionous <github@ionous.net>
2017-07-30 21:14:13 -0700

    setup rtm for testing contents

---
+++ dl/locate/locale.go
@@ -0,0 +1,40 @@
+package locate
+
+import (
+	"github.com/ionous/errutil"
+	"github.com/ionous/iffy/index"
+	"github.com/ionous/iffy/rt"
+)
+
+type Locale struct {
+	*index.Table
+}
+
+func (l *Locale) SetLocation(p, c rt.Object, now Containment) (err error) {
+	types := map[Containment]struct {
+		Parent, Child string
+	}{
+		Supports: {Parent: "container", Child: "thing"},
+		Contains: {Parent: "container", Child: "thing"},
+		Wears:    {Parent: "actor", Child: "thing"},
+		Carries:  {Parent: "actor", Child: "thing"},
+		Holds:    {Parent: "room", Child: "thing"},
+	}
+	if check, ok := types[now]; !ok {
+		err = errutil.New("relation not supported", now)
+	} else if !p.GetClass().IsCompatible(check.Parent) {
+		err = errutil.New("expected parent", check.Parent)
+	} else if !c.GetClass().IsCompatible(check.Child) {
+		err = errutil.New("expected child", check.Child)
+	} else {
+		err = l.Table.AddPair(p.GetId(), c.GetId(), func(old interface{}) (ret interface{}, err error) {
+			if c, ok := old.(Containment); ok && c != now {
+				err = errutil.New("was", c, "now", now)
+			} else {
+				ret = now
+			}
+			return
+		})
+	}
+	return
+}
--- dl/std/contents_test.go
+++ dl/std/contents_test.go
@@ -6,52 +6,18 @@ import (
 	"github.com/ionous/iffy/dl/initial"
 	"github.com/ionous/iffy/dl/locate"
 	"github.com/ionous/iffy/index"
+	"github.com/ionous/iffy/pat/patbuilder"
 	"github.com/ionous/iffy/pat/patspec"
 	"github.com/ionous/iffy/ref"
 	"github.com/ionous/iffy/ref/unique"
 	"github.com/ionous/iffy/rt"
+	"github.com/ionous/iffy/rt/printer"
+	"github.com/ionous/iffy/rtm"
 	"github.com/ionous/iffy/spec/ops"
 	"github.com/ionous/sliceOf"
 	testify "github.com/stretchr/testify/assert"
 	"testing"
 )
-
-// start looking at printing, probably a match after saying something.
-// two blocks possibly: one for the execute match, one for the statements
-// no "events" yet.
-
-type Locale struct {
-	*index.Table
-}
-
-func (l *Locale) SetLocation(p, c rt.Object, now locate.Containment) (err error) {
-	types := map[locate.Containment]struct {
-		Parent, Child string
-	}{
-		locate.Supports: {Parent: "container", Child: "thing"},
-		locate.Contains: {Parent: "container", Child: "thing"},
-		locate.Wears:    {Parent: "actor", Child: "thing"},
-		locate.Carries:  {Parent: "actor", Child: "thing"},
-		locate.Holds:    {Parent: "room", Child: "thing"},
-	}
-	if check, ok := types[now]; !ok {
-		err = errutil.New("relation not supported", now)
-	} else if !p.GetClass().IsCompatible(check.Parent) {
-		err = errutil.New("expected parent", check.Parent)
-	} else if !c.GetClass().IsCompatible(check.Child) {
-		err = errutil.New("expected child", check.Child)
-	} else {
-		err = l.Table.AddPair(p.GetId(), c.GetId(), func(old interface{}) (ret interface{}, err error) {
-			if c, ok := old.(locate.Containment); ok && c != now {
-				err = errutil.New("was", c, "now", now)
-			} else {
-				ret = now
-			}
-			return
-		})
-	}
-	return
-}
 
 func TestContents(t *testing.T) {
 	classes := ref.NewClasses()
@@ -76,61 +42,104 @@ func TestContents(t *testing.T) {
 	//t.Run("Names", func(t *testing.T) {
 	assert := testify.New(t)
 
-	// patterns, e := patbuilder.NewPatternMaster(cmds, classes,
-	// 	(*Patterns)(nil)).Build(printPatterns)
-	// assert.NoError(e)
+	patterns, e := patbuilder.NewPatternMaster(cmds, classes,
+		(*Patterns)(nil)).Build(printPatterns)
+	assert.NoError(e)
 
-	pc := Locale{index.NewTable(index.OneToMany)}
+	pc := locate.Locale{index.NewTable(index.OneToMany)}
+	type OpsCb func(c *ops.Builder)
+	type Match func(lines []string) error
+	test := func(build, exec OpsCb, match Match) (err error) {
+		var src struct{ initial.Statements }
+		if c, ok := cmds.NewBuilder(&src); !ok {
+			err = errutil.New("no buidler")
+		} else {
+			if c.Cmds().Begin() {
+				build(c)
+				c.End()
+			}
+			if e := c.Build(); e != nil {
+				err = e
+			} else {
+				var facts initial.Facts
+				if e := src.Assess(&facts); e != nil {
+					err = e
+				} else {
+					objs := objects.Build()
+					// for _, v := range facts.Values {
+					// 	if obj, ok := objs.GetObject(v.Obj); !ok {
+					// 		t.Fatal("couldnt find", v.Obj)
+					// 		break
+					// 	} else if e := obj.SetValue(v.Prop, v.Val); e != nil {
+					// 		t.Fatal(e)
+					// 		break
+					// 	}
+					// }
+					// for _, r := range facts.Relations {
+					// 	if e := relations.NewRelation(r.Name, index.NewTable(r.Type)); e != nil {
+					// 		t.Fatal(e)
+					// 		break
+					// 	}
+					// }
 
-	run := func(c *ops.Builder) {
-		c.Cmd("Location", "box", locate.Contains, "cake")
-	}
+					for _, l := range facts.Locations {
+						// in this case we're probably a command too
+						if p, ok := objs.GetObject(l.Parent); !ok {
+							err = errutil.New("unknown", l.Parent)
+							break
+						} else if c, ok := objs.GetObject(l.Child); !ok {
+							err = errutil.New("unknown", l.Child)
+							break
+						} else if e := pc.SetLocation(p, c, l.Relative); e != nil {
+							err = e
+							break
+						}
+					}
 
-	var root struct{ initial.Statements }
-	if c, ok := cmds.NewBuilder(&root); ok {
-		if c.Cmds().Begin() {
-			run(c)
-			c.End()
+				}
+
+			}
 		}
-		if e := c.Build(); assert.NoError(e) {
-			var facts initial.Facts
-			if e := root.Assess(&facts); assert.NoError(e) {
-				objs := objects.Build()
-				// for _, v := range facts.Values {
-				// 	if obj, ok := objs.GetObject(v.Obj); !ok {
-				// 		t.Fatal("couldnt find", v.Obj)
-				// 		break
-				// 	} else if e := obj.SetValue(v.Prop, v.Val); e != nil {
-				// 		t.Fatal(e)
-				// 		break
-				// 	}
-				// }
-				// for _, r := range facts.Relations {
-				// 	if e := relations.NewRelation(r.Name, index.NewTable(r.Type)); e != nil {
-				// 		t.Fatal(e)
-				// 		break
-				// 	}
-				// }
-
-				for _, l := range facts.Locations {
-					// in this case we're probably a command too
-					if p, ok := objs.GetObject(l.Parent); !ok {
-						t.Fatal(l.Parent)
-						break
-					} else if c, ok := objs.GetObject(l.Child); !ok {
-						t.Fatal(l.Child)
-						break
-					} else if e := pc.SetLocation(p, c, l.Relative); e != nil {
-						t.Fatal(e)
+		if err == nil {
+			var root struct{ rt.ExecuteList }
+			if c, ok := cmds.NewBuilder(&root); !ok {
+				err = errutil.New("no builder")
+			} else {
+				if c.Cmds().Begin() {
+					exec(c)
+					c.End()
+				}
+				if e := c.Build(); e != nil {
+					err = e
+				} else {
+					var lines printer.Lines
+					run := rtm.New(classes).Objects(objects).Patterns(patterns).Writer(&lines).Rtm()
+					if e := root.Execute(run); e != nil {
+						err = e
+					} else {
+						err = match(lines.Lines())
 					}
 				}
-				// verify relation:
-				if in, ok := pc.GetData("$box", "$cake"); assert.True(ok) {
-					assert.EqualValues(locate.Contains, in)
-				}
 			}
-
 		}
+		return
 	}
-	// })
+	//
+
+	t.Run("contains", func(t *testing.T) {
+		assert := testify.New(t)
+		e := test(func(c *ops.Builder) {
+			c.Cmd("Location", "box", locate.Contains, "cake")
+		}, func(c *ops.Builder) {
+			//
+		}, func(lines []string) (nil error) {
+			// verify relation:
+			if in, ok := pc.GetData("$box", "$cake"); assert.True(ok) {
+				assert.EqualValues(locate.Contains, in)
+			}
+			return
+		})
+		assert.NoError(e)
+	})
+
 }
