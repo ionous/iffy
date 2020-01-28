2dfe3f4adc9e59efed5b48d4c5b708919d7c1cfd
ionous <github@ionous.net>
2020-01-28 15:08:47 -0800

    merge defaults test files

--- assembly/defaults_test.go
+++ assembly/defaults_test.go
@@ -12,6 +12,105 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// TestDefaultFieldAssigment to verify default values can be assigned to kinds.
+func TestDefaultFieldAssigment(t *testing.T) {
+	if t, e := newAssemblyTest(t, memory); e != nil {
+		t.Fatal(e)
+	} else {
+		defer t.Close()
+		//
+		if e := fakeHierarchy(t.modeler, []pair{
+			{"T", ""},
+			{"P", "T"},
+			{"D", "T"},
+			{"C", "P,T"},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := fakeFields(t.modeler, []kfp{
+			{"T", "d", ephemera.PRIM_DIGI},
+			{"T", "t", ephemera.PRIM_TEXT},
+			{"T", "t2", ephemera.PRIM_TEXT},
+			{"P", "x", ephemera.PRIM_TEXT},
+			{"D", "x", ephemera.PRIM_TEXT},
+			{"C", "c", ephemera.PRIM_TEXT},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := addDefaults(t.rec, []triplet{
+			{"T", "t", "some text"},
+			{"P", "t", "override text"},
+			{"P", "t2", "other text"},
+			{"P", "x", "x in p"},
+			{"D", "x", "x in d"},
+			{"C", "c", "c text"},
+			{"C", "d", 123},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := DetermineDefaults(t.modeler, t.db); e != nil {
+			t.Fatal(e)
+		} else if e := matchDefaults(t.db, []triplet{
+			{"C", "c", "c text"},
+			{"C", "d", int64(123)}, // re: int64 -- default scanner uses https://golang.org/pkg/database/sql/#Scanner
+			{"D", "x", "x in d"},
+			{"P", "t", "override text"},
+			{"P", "t2", "other text"},
+			{"P", "x", "x in p"},
+			{"T", "t", "some text"},
+		}); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
+// TestDefaultTraitAssignment to verify default traits can be assigned to kinds.
+func TestDefaultTraitAssignment(t *testing.T) {
+	if t, e := newAssemblyTest(t, memory); e != nil {
+		t.Fatal(e)
+	} else {
+		defer t.Close()
+		//
+		if e := fakeHierarchy(t.modeler, []pair{
+			{"T", ""},
+			{"P", "T"},
+			{"Q", "T"},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := fakeTraits(t.modeler, []pair{
+			{"A", "w"},
+			{"A", "x"},
+			{"A", "y"},
+			{"B", "z"},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := fakeAspects(t.modeler, []pair{
+			{"T", "A"},
+			{"P", "B"},
+			{"Q", "B"},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := addDefaults(t.rec, []triplet{
+			{"T", "x", true},
+			{"P", "y", true},
+			{"P", "z", true},
+			//
+			{"Q", "A", "w"},
+			{"Q", "B", "z"},
+			{"Q", "w", true},
+		}); e != nil {
+			t.Fatal(e)
+		} else if e := DetermineDefaults(t.modeler, t.db); e != nil {
+			t.Fatal(e)
+		} else if e := matchDefaults(t.db, []triplet{
+			{"P", "A", "y"},
+			{"P", "B", "z"},
+			{"Q", "A", "w"},
+			{"Q", "B", "z"},
+			{"T", "A", "x"},
+		}); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
 // TestDefaultDuplicates to verify that duplicate default specifications are okay
 func TestDefaultDuplicates(t *testing.T) {
 	if t, e := newDefaultsTest(t, memory, []triplet{
