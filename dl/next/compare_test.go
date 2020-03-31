f73b6cc3957ffff826c46d32307d5388c446fe98
ionous <github@ionous.net>
2020-03-31 15:44:55 -0700

    text compare

--- dl/next/compare_test.go
+++ dl/next/compare_test.go
@@ -10,7 +10,6 @@ func TestCompareNumbers(t *testing.T) {
 	test := func(a float64, op CompareTo, b float64, res bool) {
 		var run rt.Panic
 		cmp := &CompareNum{&Number{a}, op, &Number{b}}
-
 		if ok, e := cmp.GetBool(run); e != nil {
 			t.Fatal(e)
 		} else if res != ok {
@@ -28,4 +27,24 @@ func TestCompareNumbers(t *testing.T) {
 	test(10, EqualTo{}, 1, false)
 	test(1, EqualTo{}, 10, false)
 	test(8, EqualTo{}, 8, true)
+}
+
+func TestCompareText(t *testing.T) {
+	test := func(a string, op CompareTo, b string, res bool) {
+		var run rt.Panic
+		cmp := &CompareText{&Text{a}, op, &Text{b}}
+		if ok, e := cmp.GetBool(run); e != nil {
+			t.Fatal(e)
+		} else if res != ok {
+			t.Fatal("mismatch")
+		}
+	}
+	test("Z", GreaterThan{}, "A", true)
+	test("A", GreaterThan{}, "Z", false)
+	//
+	test("marzip", LessThan{}, "marzipan", true)
+	test("marzipan", LessThan{}, "marzip", false)
+	//
+	test("bobby", EqualTo{}, "bobby", true)
+	test("bobby", EqualTo{}, "phillipa", false)
 }
