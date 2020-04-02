881c26fa7544b6ecc6124ff36e07388c34ef6421
ionous <github@ionous.net>
2020-04-02 14:07:48 -0700

    remove unneeded package
    ref created a "reflection" model -- that model will now be the db

--- ref/class/doc.go
+++
@@ -1,3 +0,0 @@
-// Package class adds an inheritance mechanism for aggregated structs.
-// note: for speed's sake we could cache results in a hidden map.
-package class
--- ref/kindOf/doc.go
+++
@@ -1,2 +0,0 @@
-// Package kindOf helps suss out the reflect.Type for common types used by iffy.
-package kindOf
