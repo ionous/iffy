41d9ac7878946a1531a84b09d95be97a5d059d75
ionous <github@ionous.net>
2020-05-12 15:54:11 -0700

    remove testify dependency

--- ref/unique/metadata_test.go
+++ ref/unique/metadata_test.go
@@ -1,20 +1,11 @@
 package unique
 
 import (
-	"github.com/stretchr/testify/suite"
 	r "reflect"
 	"testing"
 )
 
-func TestMetadata(t *testing.T) {
-	suite.Run(t, new(MetadataSuite))
-}
-
-type MetadataSuite struct {
-	suite.Suite
-}
-
-func (t *MetadataSuite) MakeMetadata(tag string) (out Metadata) {
+func makeMetadata(tag string) (out Metadata) {
 	s := Tag(r.StructTag(tag))
 	if len(s) > 0 {
 		out = make(Metadata)
@@ -23,77 +14,97 @@ func (t *MetadataSuite) MakeMetadata(tag string) (out Metadata) {
 	return
 }
 
-func (t *MetadataSuite) TestKey() {
-	m := t.MakeMetadata(`if:"id"`)
-	t.Len(m, 1)
-	t.Equal("fill", m["id"])
-}
-func (t *MetadataSuite) TestKeyValue() {
-	m := t.MakeMetadata(`if:"plural:tests"`)
-	t.Len(m, 1)
-	t.Equal("tests", m["plural"])
-}
-func (t *MetadataSuite) TestMultipleKeys() {
-	m := t.MakeMetadata(`if:"id,plural:tests"`)
-	t.Len(m, 2)
-	t.Equal("fill", m["id"])
-	t.Equal("tests", m["plural"])
-}
-func (t *MetadataSuite) TestMissingCommas() {
-	m := t.MakeMetadata(`if:"edge:test:tests"`)
-	t.Len(m, 1)
-	t.Equal("test:tests", m["edge"])
+func TestMetadata(t *testing.T) {
+	t.Run("TestKey", func(t *testing.T) {
+		m := makeMetadata(`if:"id"`)
+		if cnt := len(m); cnt != 1 {
+			t.Fatal(cnt)
+		} else if str := m["id"]; str != "fill" {
+			t.Fatal(str)
+		}
+	})
+	t.Run("TestKeyValue", func(t *testing.T) {
+		m := makeMetadata(`if:"plural:tests"`)
+		if cnt := len(m); cnt != 1 {
+			t.Fatal(cnt)
+		} else if str := m["plural"]; str != "tests" {
+			t.Fatal(str)
+		}
+	})
+	t.Run("TestMultipleKeys", func(t *testing.T) {
+		m := makeMetadata(`if:"id,plural:tests"`)
+		if cnt := len(m); cnt != 2 {
+			t.Fatal(cnt)
+		} else if str := m["id"]; str != "fill" {
+			t.Fatal(str)
+		} else if str := m["plural"]; str != "tests" {
+			t.Fatal(str)
+		}
+	})
+	t.Run("TestMissingCommas", func(t *testing.T) {
+		m := makeMetadata(`if:"edge:test:tests"`)
+		if cnt := len(m); cnt != 1 {
+			t.Fatal(cnt)
+		} else if str := m["edge"]; str != "test:tests" {
+			t.Fatal(str)
+		}
+	})
 }
 
 func TestMetaTags(t *testing.T) {
-	suite.Run(t, new(MetaTagSuite))
-}
 
-type MetaTagSuite struct {
-	suite.Suite
-}
-
-func (t *MetaTagSuite) TestInvalidTag() {
-	tag := Tag(r.StructTag(`fi:"id"`))
-	t.EqualValues("", tag)
-}
-func (t *MetaTagSuite) TestKey() {
-	tag := Tag(r.StructTag(`if:"id"`))
-	if id, ok := tag.Find("id"); t.True(ok) {
-		t.Empty(id)
-	}
-}
-func (t *MetaTagSuite) TestSimilarTag() {
-	tag := Tag(r.StructTag(`if:"iddy:5"`))
-	_, ok := tag.Find("id")
-	t.False(ok)
-}
-func (t *MetaTagSuite) TestKeyValue() {
-	tag := Tag(r.StructTag(`if:"plural:tests"`))
-	if _, ok := tag.Find("id"); t.False(ok) {
-		//
-		if res, ok := tag.Find("plural"); t.True(ok) {
-			t.Equal("tests", res)
+	t.Run("TestInvalidTag", func(t *testing.T) {
+		tag := Tag(r.StructTag(`fi:"id"`))
+		if cnt := len(tag); cnt != 0 {
+			t.Fatal(tag)
 		}
-	}
-}
-func (t *MetaTagSuite) TestMultipleKeys() {
-	tag := Tag(r.StructTag(`if:"id,other:true,plural:tests"`))
-	if id, ok := tag.Find("id"); t.True(ok) {
-		t.Empty(id)
-
-		if other, ok := tag.Find("other"); t.True(ok) {
-			t.Equal("true", other)
-			//
-			if res, ok := tag.Find("plural"); t.True(ok) {
-				t.Equal("tests", res)
-			}
+	})
+	t.Run("TestKey", func(t *testing.T) {
+		tag := Tag(r.StructTag(`if:"id"`))
+		if id, ok := tag.Find("id"); !ok {
+			t.Fatal("error")
+		} else if cnt := len(id); cnt != 0 {
+			t.Fatal(cnt)
 		}
-	}
-}
-func (t *MetaTagSuite) TestMissingCommas() {
-	tag := Tag(r.StructTag(`if:"edge:test:tests"`))
-	if res, ok := tag.Find("edge"); t.True(ok) {
-		t.Equal("test:tests", res)
-	}
+	})
+	t.Run("TestSimilarTag", func(t *testing.T) {
+		tag := Tag(r.StructTag(`if:"iddy:5"`))
+		if _, ok := tag.Find("id"); ok {
+			t.Fatal("error")
+		}
+	})
+	t.Run("TestKeyValue", func(t *testing.T) {
+		tag := Tag(r.StructTag(`if:"plural:tests"`))
+		if _, ok := tag.Find("id"); ok {
+			t.Fatal("error")
+		} else if res, ok := tag.Find("plural"); !ok {
+			t.Fatal("error")
+		} else if str := res; str != "tests" {
+			t.Fatal(str)
+		}
+	})
+	t.Run("TestMultipleKeys", func(t *testing.T) {
+		tag := Tag(r.StructTag(`if:"id,other:true,plural:tests"`))
+		if id, ok := tag.Find("id"); !ok {
+			t.Fatal("error")
+		} else if cnt := len(id); cnt != 0 {
+			t.Fatal(cnt)
+		} else if other, ok := tag.Find("other"); !ok {
+			t.Fatal("error")
+		} else if str := other; str != "true" {
+			t.Fatal(str)
+		} else if res, ok := tag.Find("plural"); !ok {
+			t.Fatal("error")
+		} else if str := res; str != "tests" {
+			t.Fatal(str)
+		}
+	})
+	t.Run("TestMissingCommas", func(t *testing.T) {
+		tag := Tag(r.StructTag(`if:"edge:test:tests"`))
+		if res, ok := tag.Find("edge"); !ok {
+			t.Fatal("error")
+		} else if str := res; str != "test:tests" {
+			t.Fatal(str)
+		}
+	})
 }
