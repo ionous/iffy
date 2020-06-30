7d7133d36025fbf815caedb2b0ae0410520d072e
ionous <github@ionous.net>
2020-06-30 11:53:34 -0700

    tables: handle null data in csv output

--- tables/dump.go
+++ tables/dump.go
@@ -1,8 +1,8 @@
 package tables
 
 import (
+	"database/sql"
 	"io"
-	"strings"
 )
 
 // ex. WriteCsv(db, os.Stdout, "select col1, col2 from table", 2)
@@ -10,16 +10,25 @@ func WriteCsv(db Query, w io.Writer, q string, cols int) (err error) {
 	if rows, e := db.Query(q); e != nil {
 		err = e
 	} else {
-		c := make([]string, cols)
+		c := make([]sql.NullString, cols)
 		cp := make([]interface{}, cols)
 		for i := 0; i < cols; i++ {
 			cp[i] = &c[i]
 		}
 		err = ScanAll(rows, func() (err error) {
-			io.WriteString(w, strings.Join(c, ","))
+			for i, col := range c {
+				if i > 0 {
+					io.WriteString(w, ",")
+				}
+				if !col.Valid {
+					io.WriteString(w, "NULL")
+				} else {
+					io.WriteString(w, col.String)
+				}
+			}
 			io.WriteString(w, "\n")
 			return
-		}, cp...)
+		}, cp...) // pass the pointers to the column strings
 	}
 	return
 }
