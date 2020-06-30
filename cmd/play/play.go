3a0c3d607137926b05c5079e2f6b96628f478369
ionous <github@ionous.net>
2020-06-30 12:06:23 -0700

    disable play utility
    it's not implemented, so lets not pretend

--- cmd/play/play.go
+++ cmd/play/play.go
@@ -1,71 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"database/sql"
-	"encoding/gob"
-	"flag"
 	"log"
-	"path/filepath"
-
-	"github.com/ionous/errutil"
-	"github.com/ionous/iffy"
-	"github.com/ionous/iffy/dl/check"
-	"github.com/ionous/iffy/qna"
-	"github.com/ionous/iffy/tables"
 )
 
 func main() {
-	var inFile string
-	flag.StringVar(&inFile, "in", "", "input file name (sqlite3)")
-	flag.Parse()
-	if e := playFile(inFile); e != nil {
-		log.Fatalln(e)
-	}
-}
-
-// open db, select tests, de-gob and run them each in turn.
-// print the results, only error on critical errors
-func playFile(inFile string) (err error) {
-	if inFile, e := filepath.Abs(inFile); e != nil {
-		err = e
-	} else if db, e := sql.Open("sqlite3", inFile); e != nil {
-		err = errutil.New("couldn't create output file", inFile, e)
-	} else {
-		defer db.Close()
-		var prog []byte
-		if e := tables.QueryAll(db,
-			//select ck.name, pg.type, pg.bytes, ck.expect
-			`select pg.bytes
-		from mdl_check as ck
-		join mdl_prog pg
-			on (pg.rowid = ck.idProg)
-		order by ck.name`,
-			func() (err error) {
-				var res check.TestOutput
-				dec := gob.NewDecoder(bytes.NewBuffer(prog))
-				if e := dec.Decode(&res); e != nil {
-					log.Println(e)
-				} else if e := runTest(&res); e != nil {
-					log.Println(e)
-				}
-				return
-			}, &prog); e != nil {
-			err = e
-		}
-	}
-	return
-}
-
-func runTest(prog check.Testing) (err error) {
-	if e := prog.RunTest(qna.NewRuntime(nil)); e != nil {
-		err = e
-	} else if e != nil {
-		err = errutil.New("unexpected failure", prog, e)
-	}
-	return
-}
-
-func init() {
-	iffy.RegisterGobs()
+	log.Fatalln("not implemented")
 }
