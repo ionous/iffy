ecb87ed1e52d125c2d3b6d27ba63e5ff37c05903
ionous <github@ionous.net>
2020-06-30 12:05:52 -0700

    composer: compact logging on load for easier debugging

--- cmd/compose/www/data/stories/loadStory.js
+++ cmd/compose/www/data/stories/loadStory.js
@@ -1,5 +1,10 @@
 function getStory() {
   const serial= localStorage.getItem("save");
   console.log(serial);
-  return serial? JSON.parse(serial): newStory();
+  const ret= serial? JSON.parse(serial): newStory();
+  const got= JSON.stringify(compact(ret), 0, 2);
+  // pull some pairs of closing parens together, and opening brackets onto the same line
+  const out= got.replace(/([}\]])\n\s+([}\]])/g, "$1$2").replace(/",\n\s+([{\[])/g, '", $1');
+  console.log(out);
+  return ret;
 }
