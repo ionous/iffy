6dabebdc2b13963e9bc26cabb24a303e03ba8a22
ionous <github@ionous.net>
2020-08-27 12:34:52 -0700

    add phrases, actions, guards to command tabs
    optimize vue observables by moving things to created()

--- cmd/compose/www/src/browseCtrl.js
+++ cmd/compose/www/src/browseCtrl.js
@@ -1,38 +1,5 @@
-// a list of { name, labels } representing commands used by the composer
-class TabList extends Map {
-  constructor({sansContent=false, txtContent=false}={}) {
-    super();
-    this.sansContent= sansContent;
-    this.txtContent= txtContent;
-  }
-  // returns { label, name }
-  outline() {
-    return Array.from(this.keys(), (k) => ({
-        name:k, label:Types.labelOf( Types.get(k) )
-     }));
-  }
-  // return { label, name } of the sub items.
-  contents(k) {
-    let ret= [];
-    if (!k || this.sansContent) {
-        ret= this.outline();
-    } else {
-      const names= this.get(k);
-      if (this.txtContent) {
-        ret= Array.from(names, (k) => ({
-            label:k
-        }));
-      } else {
-        ret= Array.from(names, (k) => ({
-            name:k, label:Types.labelOf( Types.get(k) )
-        }));
-      }
-    }
-    return ret.sort((a,b)=> a.label.localeCompare(b.label));
-  }
-}
 
-// a group of tabs, each with their own list of {name, label} commands.
+// a group of tabs, each with their own lists or maps of lists
 class Tabbable {
   constructor(lists) {
     this.finder= null; // see bind()
@@ -41,8 +8,22 @@ class Tabbable {
     this.items= [];// items => [{name, label}]
   }
   // change the active items
-  updateTab({tab, item}) {
-    this.items= this.lists[tab].contents(item);
+  updateTab(tab, item) {
+    let contents;
+    const list= this.lists[tab];
+    if (Array.isArray(list)) {
+      contents= this._outlineOf(list);
+    } else if (!item) {
+      contents= this._outlineOf(Object.keys(list));
+    } else {
+      contents= this._outlineOf(list[item]);
+    }
+    return contents.sort((a,b)=> a.label.localeCompare(b.label));
+  }
+  _outlineOf(names) {
+    return names.map((k)=> ({
+        name:k, label:Types.labelOf( Types.get(k) )
+      }));
   }
   // bind a container of items
   // each el in containerEl should have a data-drag-idx
@@ -54,18 +35,19 @@ class Tabbable {
     return false;
   }
   dragStart(el, dt) {
-    let okay;
+    let ret;
     if (this.items) {
       const start= this.finder && this.finder.findIdx(el, true); // { el, idx, edge };
       if (start) {
-        Dropper.setDragData(dt, el, this._serializeItem(start));
-        okay= true;
+        const item= this.items[start.idx];
+        start.name= item.name;
+        Dropper.setDragData(dt, el, this._serializeItem(item));
+        ret= start;
       }
     }
-    return okay;
+    return ret;
   }
-  _serializeItem(start) {
-    const item= this.items[start.idx];
+  _serializeItem(item) {
     return {
       'text/plain': item.name,
     };
@@ -79,7 +61,7 @@ Vue.component('mk-browser', {
   ><div class="mk-aux__title mk-aux__title--right"
      >commands</div
   ><div :class="bemElem('nav')"
-    ><span v-for="(x,i) in tabbable.names"
+    ><span v-for="(x,i) in tabs"
       ><template v-if="i"
       >, </template
       ><mk-a-button
@@ -89,12 +71,12 @@ Vue.component('mk-browser', {
     ></span
   >.</div
   ><div :class="bemElem('subtitle')"
-     >{{item}} {{tab|capitalize}}</div
+     >{{item|capitalize}} {{tab|capitalize}}</div
   ><ul
     class="mk-browser-list"
     ref="browserList"
     ><li
-      v-for="(k,idx) in tabbable.items"
+      v-for="(k,idx) in items"
       ><span
         v-if="k.name"
         :key="k.name"
@@ -109,97 +91,91 @@ Vue.component('mk-browser', {
   ></ul
   ></div>`,
   data(){
-    const types= allTypes.all;
-    // compile groups, slots, and strs
-    const all= new TabList({sansContent:true});
-    const groups= new TabList(); // group name => [ runs that implement the group ]
-    const slots= new TabList();
-    const str= new TabList({txtContent:true});
-    //
-    for (const typeName in types) {
-      const type= types[typeName];
-      all.set(typeName, type);
-      //
-      const group= type.group;
-      if (group) {
-        const gs= Array.isArray(group)? group: [group];
-        for (const g of gs) {
-          const typeNames= groups.get(g) || [];
-          typeNames.push(typeName);
-          groups.set(g, typeNames);
-        }
-      }
-      switch (type.uses) {
-        case 'run': {
-          const slotNames = type.with.slots || [];
-          for (const slot of slotNames) {
-            const typeNames= slots[slot] || [];
-            typeNames.push(typeName);        // add our new type
-            slots.set(slot, typeNames); // write it back
-          }
-          break;
-        }
-        case 'opt': {
-          const opts= Object.values( type.with.params ).map((x)=>x.type || x);
-          slots.set(typeName, opts);
-          break;
-        }
-        case 'str': {
-          // FIX: it feels frustrating that definitions for user types
-          // are in anyway different -- storage wise -- than custom types
-          // ie. where is our "spec" db?
-          const vals= Object.values( type.with.params ).map((x)=>x.label||x);
-          str.set(typeName, vals);
-          break;
-        }
-      };
-    }
-    const tabbable= new Tabbable({
-      all,
-      groups,
-      slots,
-      str,
+    this.$nextTick(() => {
+      this.tabs= this.tabbable.names;
+      this.items= this.tabbable.updateTab(this.tab, this.item)
     });
-    const { "$root": root } = this;
-    const dropper= root.dropper;
-    const handler= new DragHandler(dropper, tabbable);
-    this.$nextTick(function() {
-      this.onTab("all");
-    });
+    const tab= "groups"; // current tab. changed by the user.
+    const item= ""; // current item within a tab.
     return {
-      // current tab. changed by the user.
-      tab: "groups",
-      // current item within a tab.
-      item: "",
+      tab,
+      item,
+      tabs: [],
+      items: [],
       // css class helper
       cls: {
         win: [ this.bemBlock(), 'mk-aux' ],
         btnSel: this.bemElem('btn', 'sel'),
       },
-      tabbable,    // list of clickable tab names
-      handler, // drag-drop listener
     };
   },
   methods: {
     onTab(tab) {
       this.tab= tab;
       this.item= "";
-      this.tabbable.updateTab(this);
+      this.items= this.tabbable.updateTab(this.tab, this.item);
     },
     onItem(item) {
       if (!this.item) {
         this.item= item; // selected a sub-group
       }
-      this.tabbable.updateTab(this);
+      this.items= this.tabbable.updateTab(this.tab, this.item);
       this.$root.cmdSelected(item);
     },
     onNodeSelected(node, param, token) {
       // FIX: synchronize browser display
     },
   },
+  created() {
+    const types= allTypes.all;
+    // compile groups, slots, and strs
+    const all= [];
+    const groups= {};
+    const slots= {};
+
+    for (const typeName in types) {
+      const type= types[typeName];
+      all.push(typeName);
+      //
+      const group= type.group;
+      if (group) {
+        const gs= Array.isArray(group)? group: [group];
+        for (const g of gs) {
+          const els= groups[g] || [];
+          els.push(typeName);
+          groups[g]= els;
+        }
+      }
+      if (type.uses==='run') {
+        const slotNames = type.with.slots || [];
+        for (const slot of slotNames) {
+          const els= slots[slot] || [];
+          els.push(typeName);        // add our new type
+          slots[slot]= els;
+        }
+      }
+    }
+
+    const phrases= slots["story_statement"];
+    const guards= slots["bool_eval"];
+    const actions= slots["execute"];
+    const tabbable= new Tabbable({
+      all,
+      phrases,
+      actions,
+      guards,
+      groups,
+      slots,
+    });
+    this.tabbable= tabbable;
+  },
   mounted() {
-    this.$root.$on("node-selected", this.onNodeSelected);
-    this.handler.listen(this.$refs.browserList);
+    const { "$root": root, "$refs": refs, tabbable } = this;
+    const handler= new DragHandler(root.dropper, tabbable);
+    root.$on("node-selected", this.onNodeSelected);
+    handler.listen(refs.browserList);
+    //
+    this.handler= handler; // for "silence"
   },
   beforeDestroy() {
     this.$root.$off("node-selected", this.onNodeSelected);
