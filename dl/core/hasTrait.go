991636bfbb9d8944b436268df82422e510a77787
ionous <github@ionous.net>
2020-09-22 12:50:24 -0700

    bug fix cmd has trait
    it should be checking for "is" trait -- not does the trait exist at all

--- dl/core/hasTrait.go
+++ dl/core/hasTrait.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"github.com/ionous/iffy/dl/composer"
-	"github.com/ionous/iffy/object"
 	"github.com/ionous/iffy/rt"
 )
 
@@ -18,23 +17,21 @@ func (*HasTrait) Compose() composer.Spec {
 		Name:  "has_trait",
 		Spec:  "{object%obj:object_ref} is {trait:text_eval}",
 		Group: "objects",
-		Desc:  "Has Trait: Return true if the object current is using the named trait.",
+		Desc:  "Has Trait: Return true if noun is currently in the requested state.",
 	}
 }
 
 func (op *HasTrait) GetBool(run rt.Runtime) (ret bool, err error) {
-	if obj, e := rt.GetText(run, op.Obj); e != nil {
-		err = e
+	if id, e := GetObjectRef(run, op.Obj); e != nil {
+		err = cmdError(op, e)
 	} else if trait, e := rt.GetText(run, op.Trait); e != nil {
-		err = e
-	} else if p, e := run.GetField(obj+"."+trait, object.Aspect); e != nil {
-		err = e
-	} else if aspect, e := p.GetText(run); e != nil {
-		err = e
-	} else if currTrait, e := run.GetField(obj, aspect); e != nil {
-		err = e
-	} else if currTrait, e := currTrait.GetText(run); e != nil {
-		ret = trait == currTrait
+		err = cmdError(op, e)
+	} else if p, e := run.GetField(id, trait); e != nil {
+		err = cmdError(op, e)
+	} else if ok, e := p.GetBool(run); e != nil {
+		err = cmdError(op, e)
+	} else {
+		ret = ok
 	}
 	return
 }
