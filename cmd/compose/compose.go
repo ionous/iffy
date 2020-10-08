2c7c3eb78e8b30bb5a49a371af1c4db743fdf825
ionous <github@ionous.net>
2020-10-07 21:53:50 -0700

    composer: auto open browser

--- cmd/compose/compose.go
+++ cmd/compose/compose.go
@@ -5,18 +5,25 @@ import (
 	"go/build"
 
 	"github.com/ionous/iffy/composer"
+	"github.com/ionous/iffy/web/support"
 )
 
-// ex. go run compose.go -dir /Users/ionous/Documents/Iffy
+// ex. go run compose.go -open -dir /Users/ionous/Documents/Iffy
 // needs a subdirectory "stories"
 func main() {
 	var dir string
+	var open bool
 	flag.StringVar(&dir, "dir", "", "directory for processing iffy files.")
+	flag.BoolVar(&open, "open", false, "open a new browser window.")
 	flag.Parse()
 	//
 	cfg := composer.DevConfig(build.Default.GOPATH)
 	if len(dir) > 0 {
 		cfg.Root = dir
 	}
+	if open {
+		support.OpenBrowser("http://localhost:3000/compose/")
+	}
+	// by design, this never returns.
 	composer.Compose(cfg)
 }
