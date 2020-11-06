0ad19fa2dabeeecbfe3c682ff2868566fd3f22f2
ionous <github@ionous.net>
2020-11-06 12:56:18 -0800

    share generic interator construction

--- rt/generic/sliceIt.go
+++ rt/generic/sliceIt.go
@@ -1,43 +1,18 @@
 package generic
 
-import "github.com/ionous/iffy/rt"
+import (
+	"github.com/ionous/iffy/rt"
+	"github.com/ionous/iffy/rt/chain"
+)
 
-func SliceIt(size int, next func(int) rt.Value) *sliceIt {
-	return &sliceIt{size: size, next: next}
-}
-
-func SliceFloats(vs []float64) *sliceIt {
-	return SliceIt(len(vs), func(i int) rt.Value {
-		return NewFloat(vs[i])
+func SliceFloats(vs []float64) rt.Iterator {
+	return chain.SliceIt(len(vs), func(i int) (rt.Value, error) {
+		return NewFloat(vs[i]), nil
 	})
 }
 
-func SliceStrings(vs []string) *sliceIt {
-	return SliceIt(len(vs), func(i int) rt.Value {
-		return NewString(vs[i])
+func SliceStrings(vs []string) rt.Iterator {
+	return chain.SliceIt(len(vs), func(i int) (rt.Value, error) {
+		return NewString(vs[i]), nil
 	})
-}
-
-type sliceIt struct {
-	at   int
-	size int
-	next func(int) rt.Value
-}
-
-func (it *sliceIt) Remaining() int {
-	return it.size - it.at
-}
-
-func (it *sliceIt) HasNext() bool {
-	return it.at < it.size
-}
-
-func (it *sliceIt) GetNext() (ret rt.Value, err error) {
-	if !it.HasNext() {
-		err = rt.StreamExceeded
-	} else {
-		ret = it.next(it.at)
-		it.at++
-	}
-	return
 }
