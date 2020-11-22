db08817edac3d4e534c2e21626de995db1216318
ionous <github@ionous.net>
2020-11-21 16:39:06 -0800

    Update zero.go

--- rt/generic/zero.go
+++ rt/generic/zero.go
@@ -1,12 +1,10 @@
 package generic
 
 var (
-	True      = BoolOf(true)
-	False     = BoolOf(false)
-	Zero      = FloatOf(0.0)
-	ZeroList  = FloatsOf(nil)
-	Empty     = StringOf("")
-	EmptyList = StringsOf(nil)
+	True  = BoolOf(true)
+	False = BoolOf(false)
+	Zero  = FloatOf(0.0)
+	Empty = StringOf("")
 )
 
 const defaultType = "" // empty string
