51b5ce40fdfbaee47da684a4ff566e38064dbfa3
ionous <github@ionous.net>
2020-12-23 21:54:36 -0800

    allow all checks to run

--- qna/checkAll.go
+++ qna/checkAll.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/gob"
-	"log"
 
 	"github.com/ionous/errutil"
 
@@ -29,22 +28,21 @@ func CheckAll(db *sql.DB, actuallyJustThisOne string) (ret int, err error) {
 				var curr check.CheckOutput
 				dec := gob.NewDecoder(bytes.NewBuffer(prog))
 				if e := dec.Decode(&curr); e != nil {
-					log.Println(e)
+					err = e
 				} else {
 					tests = append(tests, curr)
 				}
 			}
-			return nil // we log rather than return error
+			return
 		}, &name, &prog); e != nil {
-		err = e
+		err = errutil.New("query error", e)
 	} else if len(tests) == 0 {
 		err = errutil.New("no matching tests found")
 	} else {
 		// FIX: we have to cache the statements b/c we cant use them during QueryAll
 		for _, t := range tests {
 			if e := t.RunTest(run); e != nil {
-				err = e
-				break
+				err = errutil.Append(err, e)
 			}
 			ret++
 		}
