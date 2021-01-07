0828a74bc85718797a2dc44ffe9143d314d158c2
ionous <github@ionous.net>
2021-01-06 21:20:07 -0800

    fix bad whitespace in go file.
    sublime vs gofmt. fight.

--- ephemera/decode/decoder_test.go
+++ ephemera/decode/decoder_test.go
@@ -1,24 +1,24 @@
 package decode
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/ionous/iffy/dl/core"
-  "github.com/ionous/iffy/ephemera/debug"
-  "github.com/kr/pretty"
+	"github.com/ionous/iffy/dl/core"
+	"github.com/ionous/iffy/ephemera/debug"
+	"github.com/kr/pretty"
 )
 
 // read simple unit test story into memory as a golang struct
 func TestDecode(t *testing.T) {
-  dec := NewDecoder()
-  // register creation functions for all the slats.
-  dec.AddDefaultCallbacks(core.Slats)
-  // read say story data
-  if prog, e := dec.ReadSpec(debug.SayHelloGoodbyeData); e != nil {
-    t.Fatal(e)
-  } else if diff := pretty.Diff(debug.SayHelloGoodbye, prog); len(diff) > 0 {
-    t.Fatal(diff)
-  } else {
-    t.Log(pretty.Sprint(prog))
-  }
+	dec := NewDecoder()
+	// register creation functions for all the slats.
+	dec.AddDefaultCallbacks(core.Slats)
+	// read say story data
+	if prog, e := dec.ReadSpec(debug.SayHelloGoodbyeData); e != nil {
+		t.Fatal(e)
+	} else if diff := pretty.Diff(debug.SayHelloGoodbye, prog); len(diff) > 0 {
+		t.Fatal(diff)
+	} else {
+		t.Log(pretty.Sprint(prog))
+	}
 }
